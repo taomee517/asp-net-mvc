185b4701f77a7c2e45ebc270e0f2ce0eef2a88a6
Administrator <taomee517@gmail.com>
2020-08-09 12:26:54 +0800

    log4net 连接kafka

--- asp-net-mvc/Program.cs
+++ asp-net-mvc/Program.cs
@@ -7,7 +7,7 @@ namespace asp_net_mvc
     public class Program
     {
         //.net core项目配置log4net日志： https://www.jianshu.com/p/d371c98c2def
-        //type 是指你当前使用log的对象的类，例如需要在HomeController中使用，参数就为typeof(HomeController)
+        // asp.netcore Log4Net连接kafka的方法: https://www.cnblogs.com/puzi0315/p/11429817.html
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
